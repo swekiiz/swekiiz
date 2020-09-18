db70376859a550ea14a727aeb5f85d9831a753c8
swekiiz <best_n.dol@hotmail.com>
2020-09-19 02:30:33 +0700

    Test

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 - 💻 I’m currently learning computer engineering.
 
-![Image of Yaktocat](https://swekiiz.vercel.app/assets/catTyping.gif)
+![Image of Yaktocat](https://swekiiz.vercel.app/api)
 
 <!--
 **swekiiz/swekiiz** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,18 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 
-export default (req: NowRequest, res: NowResponse) => {
-  return res.json({ message: "Hello World" });
-};
+const fs = require("fs");
+
+export default async function (req: NowRequest, res: NowResponse) {
+  try {
+    const gif = fs.createReadStream(`${__dirname}/../assets/catTyping.gif`);
+    res.setHeader("Content-Type", "image/svg+xml");
+    gif.pipe(res);
+    // console.log(res);
+
+    // console.log("Pass");
+  } catch (error) {
+    res.send("Error");
+    console.error(error);
+    // console.log("Error");
+  }
+}
